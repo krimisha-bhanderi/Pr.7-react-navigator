6afdfa82b1155f52af1d2ffec78fae13ddaacee5
krimisha <krimisha025@gmail.com>
2024-06-11 11:07:31 +0530

    pr

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import View from './View'
+import Add from './Add'
+import Edit from './Edit'
+
+const Home = () => {
+  return (
+   <>
+   
+        <View/>
+        <Add/>
+        <Edit/>
+   </>
+  )
+}
+
+export default Home
